388a9bee452891f3fb6fda5f45022ce07f40d955
Andrew <50529632+realPointer@users.noreply.github.com>
2024-12-15 02:13:56 +0300

    feat: complete task 22

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -366,8 +366,10 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str.replace(/[a-z]/gi, (char) =>
+    char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+  );
 }
 
 /**
