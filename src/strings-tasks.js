72b828f6c5625e9a1ee60628151e6b1db08af199
Andrew <50529632+realPointer@users.noreply.github.com>
2024-12-15 01:43:33 +0300

    feat: complete task 11

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -184,8 +184,10 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  return str
+    ? str.split('').reduce((sum, char) => sum + char.charCodeAt(0), 0)
+    : 0;
 }
 
 /**
